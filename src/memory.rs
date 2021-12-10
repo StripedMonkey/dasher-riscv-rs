ee8044cd0eb56fb1734031c5bc7766c8876da16b
StripedMonkey <monkeyinastripedshirt@gmail.com>
2021-12-09 23:28:24 -0500

    refactor memory to avoid rwlocks for now

--- src/memory.rs
+++ src/memory.rs
@@ -1,22 +1,22 @@
 use byteorder::{ByteOrder, LittleEndian};
 use std::{sync::RwLock, vec};
 pub(crate) struct MemorySystem {
-    ram: RwLock<Vec<i8>>,
-    video_memory: RwLock<Vec<i8>>,
+    ram: Vec<i8>,
+    video_memory: Vec<i8>,
 }
 
 impl MemorySystem {
     pub fn new() -> Self {
         Self {
-            ram: RwLock::new(vec![0; 524_288 * 4]),
-            video_memory: RwLock::new(vec![0; 2048 * 4]),
+            ram: vec![0; 524_288 * 4],
+            video_memory: vec![0; 2048 * 4],
         }
     }
     pub fn read_i32(&self, idx: usize) -> i32 {
         match idx {
             0x00000000..=0x0001FFFF => {
                 // RAM
-                let contents = self.ram.read().unwrap();
+                let contents = &self.ram;
                 read_slice(&contents[idx..(idx + 5)])
             }
             0x00020000..=0x3FFFFFFF => {
@@ -25,7 +25,7 @@ impl MemorySystem {
             }
             0x40000000..=0x400007FF => {
                 // Video RAM
-                let contents = self.ram.read().unwrap();
+                let contents = &self.ram;
                 read_slice(&contents[idx..(idx + 5)])
             }
             0x40000800..=0x7FFFFFFF => {
@@ -48,7 +48,7 @@ impl MemorySystem {
         match idx {
             0x00000000..=0x0001FFFF => {
                 // RAM
-                let mut contents = self.ram.write().unwrap();
+                let mut contents = &mut self.ram;
                 write_slice(&mut contents[idx..(idx + 5)], data);
             }
             0x00020000..=0x3FFFFFFF => {
@@ -57,7 +57,7 @@ impl MemorySystem {
             }
             0x40000000..=0x400007FF => {
                 // Video RAM
-                let mut contents = self.video_memory.write().unwrap();
+                let mut contents = &mut self.video_memory;
                 write_slice(&mut contents[idx..(idx + 5)], data);
             }
             0x40000800..=0x7FFFFFFF => {
@@ -82,14 +82,14 @@ impl MemorySystem {
         // There's probably a more eloquent way to write this
         let mut dump: Vec<i32> = Vec::new();
         if start < 0x00020000 && stop < 0x00020000 {
-            let contents = self.ram.read().unwrap();
+            let contents = &self.ram;
             for i in (start..stop).step_by(4) {
                 dump.push(read_slice(&contents[i..(i + 5)]));
             }
             return Ok(dump);
         }
         if start < 0x40000000 && stop < 0x40000000 {
-            let contents = self.video_memory.read().unwrap();
+            let contents = &self.video_memory;
             for i in (start..stop).step_by(4) {
                 dump.push(read_slice(&contents[i..(i + 5)]));
             }
